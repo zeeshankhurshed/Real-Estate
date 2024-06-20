a5fd75b50a88477a725958acff2e5fc7b29d4335
zeeshankhurshed <109623408+zeeshankhurshed@users.noreply.github.com>
2024-06-20 05:22:12 +0500

    full real-estate project

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -47,7 +47,7 @@ const Home = () => {
   return (
     <>
       {/* top */}
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+      <div className=' flex flex-wrap justify-between items-center'>
         <h2 className='text-slate-700 font-bold text-3xl lg:text-6xl'>Find Your next <span className='text-slate-500'>perfect</span> <br />place with ease</h2>
         <p className='text-gray-400 text-xs sm:text-sm'>Zee Estate will help you find your next perfect place to live. <br /> We have a wide range of properties for you to choose from.</p>
         <Link to={'/search'} className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'>
